97cac6fd288f8e97ac8231c1087786b904e1681d
Pingaksh Gaur <pingaksh.gaur@elecbits.in>
2026-07-11 18:09:17 +0530

    PATCH UPDATE - Fixing build_file.sh

--- build_files.sh
+++ build_files.sh
@@ -7,16 +7,13 @@ pip install -r requirements.txt
 echo "STEP 2: checking Django config"
 python manage.py check
 
-echo "STEP 2: making migrations"
-python manage.py makemigrations accounts jobs tasks
-
-echo "STEP 4: running migrations"
+echo "STEP 3: running migrations"
 python manage.py migrate --no-input
 
-echo "STEP 5: creating/updating admin user"
+echo "STEP 4: creating/updating admin user"
 python manage.py bootstrap_admin
 
-echo "STEP 6: collecting static files"
+echo "STEP 5: collecting static files"
 python manage.py collectstatic --no-input
 
 echo "BUILD SCRIPT FINISHED"
