a2d75d59a6c195abf7778b863e47c19b72166966
Pingaksh Gaur <pingaksh.gaur@elecbits.in>
2026-07-11 22:50:44 +0530

    Refix Path Update

---
+++ apps/resumes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ResumeListView, ResumeUploadView, resume_inline_upload
+
+app_name = "resumes"
+
+urlpatterns = [
+    path("", ResumeListView.as_view(), name="list"),
+    path("upload/", ResumeUploadView.as_view(), name="upload"),
+    path("inline-upload/", resume_inline_upload, name="inline-upload"),
+]
