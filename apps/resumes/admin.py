c965ab474c6dd6fb10aff1ecef54c7f431cbb0cd
Pingaksh Gaur <pingaksh.gaur@elecbits.in>
2026-07-11 22:07:57 +0530

    Some handmade changes in shadows, transitions and Headers

--- apps/resumes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ResumeFile
-
-
-@admin.register(ResumeFile)
-class ResumeFileAdmin(admin.ModelAdmin):
-    list_display = ('original_filename', 'application', 'file_size', 'created_at')
-    search_fields = ('original_filename', 'application__full_name', 'application__email')
